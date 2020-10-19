07d2140fc9a4446800d54beff492f5277236d6c2
MC3D <m.chapman.home@gmail.com>
2020-10-19 15:20:02 -0400

    adds psychopg2-binary

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1:8000',
+    'limitless-mountain-49530.herokuapp.com',
+]
 
 
 # Application definition
@@ -138,3 +141,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/static/build/static'),)
 REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend/static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
